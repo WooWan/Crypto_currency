6f64f1c4cd46c55c609477095bb4ad5781f9c65e
WooWan <woohobi@gmail.com>
2021-06-14 20:56:16 +0900

    Update final

--- coins/bitcoin/urls.py
+++ coins/bitcoin/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='home')
+    path('bitcoin', views.bitcoin, name='bitcoin'),
+    path('ethereum', views.ethereum, name='ethereum'),
 ]
--- coins/coins/urls.py
+++ coins/coins/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bitcoin/', include('bitcoin.urls')),
+    path('coin/', include('bitcoin.urls')),
 ]
